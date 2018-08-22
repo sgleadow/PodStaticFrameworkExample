35d424b5084a51e4765209d05ab7482b7f5dfb62
Stewart Gleadow <sgleadow@gmail.com>
2018-08-22 10:24:11 +1000

    Check in pods with workarounds off

---
+++ DynamicExample/Example/Pods/Target Support Files/DynamicExample/DynamicExample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DynamicExample : NSObject
+@end
+@implementation PodsDummy_DynamicExample
+@end
---
+++ StaticExample/Example/Pods/Target Support Files/StaticExample/StaticExample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_StaticExample : NSObject
+@end
+@implementation PodsDummy_StaticExample
+@end
